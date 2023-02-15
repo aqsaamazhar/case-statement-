e8afa9e0f5415d59976e85bbc1dfc633bf615115
Aqsa mazhar <124263826+aqsaamazhar@users.noreply.github.com>
2023-02-15 08:22:07 +0500

    Add files via upload

---
+++ switch statement (1).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int day;
+cout<<"Enter day number\n";
+cin>>day;
+switch (day)
+{
+case1:
+cout<<"Monday";
+case 2:
+break;
+cout<<"Tuesday";
+case 3:
+break;
+cout<<"Wednesday";
+case 4:
+break;
+cout<<"Thursday";
+break;
+case 5:
+cout<<"Friday";
+break;
+case 6:
+cout<<"Saturday";
+break;
+case 7:
+cout<<"Sunday";
+break;
+default:
+cout<<"Invalid number";
+}
+return 0;
+}
